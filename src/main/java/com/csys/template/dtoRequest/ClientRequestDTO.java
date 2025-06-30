6700caea62c8fa92456ed07da4fdb2cc98a6140a
Yasmin Jmal <yasminjmal32@gmail.com>
2025-06-30 11:26:07 +0100

    Update ClientRequestDTO.java

--- src/main/java/com/csys/template/dtoRequest/ClientRequestDTO.java
+++ src/main/java/com/csys/template/dtoRequest/ClientRequestDTO.java
@@ -1,31 +1,23 @@
 package com.csys.template.dtoRequest;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 public class ClientRequestDTO {
 
-    @NotNull(message = "Le nom du client ne peut pas être nul")
-    @Size(min = 1, max = 100, message = "Le nom du client doit contenir entre 1 et 100 caractères")
     private String nomComplet;
 
-    @Size(max = 200, message = "L'adresse ne peut pas dépasser 200 caractères")
     private String adress;
 
     @Email(message = "Veuillez fournir une adresse email valide")
-    @Size(max = 100, message = "L'email ne peut pas dépasser 100 caractères")
     private String email;
 
     // REMPLACÉ : private String region;
     // PAR :
-    @Size(max = 10, message = "Le code pays ne peut pas dépasser 10 caractères")
     private String countryCode; // Ex: "TN", "FR", "US"
 
-    @Size(max = 100, message = "Le nom de la région ne peut pas dépasser 100 caractères")
     private String regionName; // Ex: "Sfax", "Île-de-France", "California"
 
-    @NotNull(message = "Le statut Actif ne peut pas être nul")
     private Boolean actif;
 
     // Getters and Setters
